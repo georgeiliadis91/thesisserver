cff2fecca9bb4ef64eb4c7879ea3ddfee4a5285f
George Iliadis <you@example.com>
2019-10-31 10:53:21 +0000

    Backend Core finished

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
+	firebase_id: {
+		type: String,
+		required: true
+	},
 	name: {
 		type: String,
 		required: true
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const mongoose = require('mongoose')
 
@@ -9,7 +10,7 @@ const db = mongoose.connection
 
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
-
+app.use(cors());
 app.use(express.json())
 
 const quizRouter = require('./routes/quizes')
@@ -18,4 +19,5 @@ app.use('/quizes', quizRouter)
 const usersRouter = require('./routes/users')
 app.use('/users', usersRouter)
 
-app.listen(3000, () => console.log('Server Started'))
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log(`Server started, listening to port ${port}`));
